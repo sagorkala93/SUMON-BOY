7522fb731ebf6e8735fffe335cdfbc4be721047e
sagorkala93 <sskjibon1026@gmail.com>
2026-03-01 00:12:25 +0600

    Update Hot.js

--- Script/commands/Hot.js
+++ Script/commands/Hot.js
@@ -143,7 +143,7 @@ module.exports.run = async function({ api, event }) {
     .pipe(fs.createWriteStream(path))
     .on("close", () => {
       api.sendMessage({
-        body: "🔰পাপির দল😤\nহাত মারবি না কিন্তু🥵🫵",
+        body: "🔰সুমন বলছি😤\nহাত মারবি না কিন্তু🥵🫵",
         attachment: fs.createReadStream(path)
       }, event.threadID, () => fs.unlinkSync(path));
     })
